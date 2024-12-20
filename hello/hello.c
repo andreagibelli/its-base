f8088e6f85c1fb588603e96fbd3604b111fbe558
andreagibelli <andrea.gibe2005@gmail.com>
2024-12-20 16:09:40 +0000

    hello.c update

--- hello/hello.c
+++ hello/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("hello word\n");
+    printf("hello word\n\n metti il nome ");
+    char name[32];
+    scanf("%s", name);
+    printf("\thello %s\n\n\n", name);
     return 0;
 }
