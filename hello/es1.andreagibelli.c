d9ec3bce6f7fb07c2eec053dd1de6ad8bb1d338f
andreagibelli <andrea.gibe2005@gmail.com>
2025-01-15 16:59:00 +0000

    ciao

--- hello/es1.andreagibelli.c
+++ hello/es1.andreagibelli.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
 	char nome[40], cognome[40];
@@ -7,6 +8,6 @@ int main()
 	printf("\ninserisci il tuo cognome: ");
 	scanf("%s", cognome);
 	printf("\nbenvenuto, %s %s!\nGrazie per aver usato il nostro programma.\nTi auguriamo Buone Feste", nome, cognome);
-	printf("\n\t   *\n\t  ***\n\t *****\n\t*******\n\t  ***");
+	printf("\n\t   *\n\t  ***\n\t *****\n\t*******\n\t  ***\n\n\n\n\n\n");
 	return 0;
 }
